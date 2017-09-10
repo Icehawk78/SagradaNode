bdce44ca692c95d84e22a74d7d415b238818f904
smartshaila <smartshaila@gmail.com>
2017-09-10 10:55:32 -0500

    Board model object things

--- services/board.js
+++ services/board.js
@@ -5,14 +5,25 @@ var width = 5;
 var height = 4;
 
 board.initialize = function() {
-    return _.times(width, (x) => {
-        return _.times(height, (y) => {
-            return {
-                x: x,
-                y: y
-            };
-        });
-    });
+    return {
+        rows: _.times(width, (x) => {
+            return _.times(height, (y) => {
+                return {
+                    x: x,
+                    y: y,
+                    rules: {color: null, number: null},
+                    die: {color: null, number: null}
+                };
+            });
+        }),
+        valid_dice: valid_dice
+    };
 };
+
+function valid_dice(x, y) {
+    let self = this;
+    return self.rows[x][y];
+}
+
 
 module.exports = board;
