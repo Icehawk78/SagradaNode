2d4df04a2441a880e7cd0f410daeb5728da17d11
Nicholas <i.am@aweso.ms>
2017-09-24 08:53:45 -0500

    Add skeleton for game

--- routes/board.js
+++ routes/board.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let router = express.Router();
 let board = require('../services/board');
+let game = require('../services/game');
 
 let my_board = board.initialize('blue');
 my_board.randomize_rules();
+
+let my_game = game.initialize(2);
 
 router.get('/', (req, res, next) => {
     res.render('board', {title: 'Sagrada Tech Demo', board: my_board});
@@ -13,6 +16,20 @@ router.post('/die', (req, res, next) => {
     let response = my_board.place_die(req.body.die, parseInt(req.body.x), parseInt(req.body.y));
     console.log(my_board.calculate_score());
     res.send(response);
+});
+
+router.get('/game', (req, res, next) => {
+    console.log(my_game);
+    res.send(my_game);
+});
+
+router.get('/:playerId', (req, res, next) => {
+    let id = parseInt(req.params.playerId);
+    if (my_game.players.length < id) {
+        res.send(false);
+    } else {
+        res.render('board', {title: 'Sagrada Tech Demo, Player #' + id, board: my_game.players[id].board});
+    }
 });
 
 module.exports = router;
--- services/game.js
+++ services/game.js
@@ -1,5 +1,26 @@
-var game = {};
+let _ = require('lodash');
+let board = require('./board');
 
+const COLORS = ['blue', 'red', 'green', 'yellow', 'purple'];
+// const PIPS = ['1', '2', '3', '4', '5', '6'];
+let game = {};
 
+game.initialize = function(player_count) {
+    let remaining_colors = _.clone(COLORS);
+    return {
+        players: _.times(player_count, (id) => {
+            let player_color = _.sample(remaining_colors);
+            _.pull(remaining_colors, player_color);
+            console.log(remaining_colors);
+            let player_board = board.initialize(player_color);
+            player_board.randomize_rules();
+            return {
+                id: id,
+                board: player_board
+            }
+        }),
+        all_dice: _.flatten(_.times(4 * player_count, () => _.clone(COLORS)))
+    };
+};
 
 module.exports = game;
